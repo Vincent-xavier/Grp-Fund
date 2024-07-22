f6d2f46fd2c276bcb4671baec89af83d65a7b91d
Vincent Xavier <vincentxavier2602@gmail.com>
2024-07-22 23:22:03 +0530

    Added Onboarding screen

---
+++ GrpFund/app/index.tsx
@@ -0,0 +1,81 @@
+import { StyleSheet } from "react-native";
+import React from "react";
+
+//Onboarding package
+import Onboarding from "react-native-onboarding-swiper";
+import LottieView from "lottie-react-native";
+import { router } from "expo-router";
+
+const OnboardingScreen = () => {
+  const handleSkip = () => {
+    router.replace("/(tabs)");
+  };
+  return (
+    <Onboarding
+      bottomBarHighlight={false}
+      showDone={true}
+      showNext={true}
+      showSkip={true}
+      onDone={() => {
+        handleSkip();
+      }}
+      onSkip={() => {
+        handleSkip();
+      }}
+      pages={[
+        {
+          backgroundColor: "#fff",
+          image: (
+            <LottieView
+              autoPlay
+              style={styles.lottie}
+              source={require("../assets/animations/onboarding1.json")}
+            />
+          ),
+          title: "Welcome to Grp Funds!",
+          subtitle:
+            "Ready to make managing group funds simple and stress-free? Let's get started!",
+        },
+        {
+          backgroundColor: "#fff",
+          image: (
+            <LottieView
+              autoPlay
+              style={styles.lottie}
+              source={require("../assets/animations/onboarding3.json")}
+            />
+          ),
+          title: "Collect Contributions",
+          subtitle:
+            "Invite members to contribute to the group fund with just a few taps. Transparent and hassle-free!",
+        },
+        {
+          backgroundColor: "#fff",
+          image: (
+            <LottieView
+              autoPlay
+              style={styles.lottie}
+              source={require("../assets/animations/onboarding2.json")}
+            />
+          ),
+
+          title: "Track Expenses",
+          subtitle:
+            "Keep a record of every transaction. Ensure everyone knows where the money goes, every step of the way.",
+        },
+      ]}
+    />
+  );
+};
+
+export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+  imageContainerStyles: {
+    padding: 0,
+  },
+  lottie: {
+    width: 240,
+    height: 240,
+  },
+});
